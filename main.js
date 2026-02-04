676fbc8411629a6cb83a58960bd304b1cb8847d5
Alvaro Ruiz <ar102rc@gmail.com>
2026-02-04 14:56:51 -0500

    Agregue comentarios para facilitar cada cosa del proyecto

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-
-const myLibrary = [];
-
+//Array de la biblioteca
+let myLibrary = [];
+//Variables de cada label
 const libro = document.querySelector("#name");
 const autor = document.querySelector("#autor");
 const genero = document.querySelector("#genero");
 const paginas = document.querySelector("#paginas");
 const leido = document.querySelector("#leido");
-
+//Variables del formulario y contenedor
 const contenedor = document.querySelector("#contenedor");
 const formulario = document.querySelector("form");
 
-
+//Constructor para crear cada libro
 function Book(book, author, genre, pages, read){
     this.book = book;
     this.author = author;
     this.genre = genre;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
 }
 
+//Prototipo para el checkbox, asi no deba de salir true/false
 Book.prototype.leer = function(){
     if (this.read === true){
         return `Si lo has leido`;
@@ -27,11 +29,13 @@ Book.prototype.leer = function(){
     };
 }
 
+//Funcion para agregar el nuevo libro al array
 function addBookLibrary(book, author, genre, pages, read){
     const biblioteca = new Book(book, author, genre, pages, read);
     myLibrary.push(biblioteca);
 }
 
+//El boton, capturar los datos y grabarlos como se debe
 formulario.addEventListener("submit" , (e) => {
     e.preventDefault();
     const book = libro.value;
@@ -40,7 +44,6 @@ formulario.addEventListener("submit" , (e) => {
     const pages = paginas.value;
     const read = leido.checked;
     
-
     addBookLibrary(book,author,genre,pages,read);
     crearTarjeta();
 
@@ -51,24 +54,42 @@ formulario.addEventListener("submit" , (e) => {
 function crearTarjeta(){
     contenedor.textContent = "";
     myLibrary.forEach(item => {
+    //Espacios para las tarjetas, sus respectivos divs por datos
     const tarjeta = document.createElement('div');
     const libro = document.createElement("div");
-    libro.textContent = item.book;
     const autor = document.createElement('div');
-    autor.textContent = item.author;
     const genero = document.createElement('div');
-    genero.textContent = item.genre;
     const paginas = document.createElement('div');
-    paginas.textContent = `${item.pages} paginas`;
     const visto = document.createElement('div');
+    const borrar = document.createElement('button');
+
+    //Asignar cada variables nueva (los divs) con el contenido colocado por el usuario
+    tarjeta.setAttribute("data-id", item.id);
+    libro.textContent = item.book;
+    autor.textContent = item.author;
+    genero.textContent = item.genre;
+    paginas.textContent = `${item.pages} paginas`;
     visto.textContent = item.leer();
+    borrar.textContent = 'Borrar';
+
+    //Funcionalidad del boton borrar
+    borrar.addEventListener("click", () =>{
+        myLibrary = myLibrary.filter(actual => actual.id !== item.id);
+        crearTarjeta();
+    })
+
+    //Visualizacion inicial de los datos insertados
     tarjeta.appendChild(libro);
     tarjeta.appendChild(autor);
     tarjeta.appendChild(genero);
     tarjeta.appendChild(paginas);
     tarjeta.appendChild(visto);
-    
+    tarjeta.appendChild(borrar);
+
+    //Orden de los datos para cada tarjeta tenga su propio espacio
     contenedor.appendChild(tarjeta);
+
+    //Estilos de las tarjetas, contenedores y del formulario
     contenedor.classList.add('contenedor');
     tarjeta.classList.add('card');
     libro.classList.add('titulo');
